53ba8cfc9f2be734801a2d247eb9247ce6e6e3c0
Michael <mishania.f@gmail.com>
2019-10-22 12:14:27 +0300

    welldone

---
+++ build/precache-manifest.b45ef78a71f016da63f59541945476d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd92e77879aea93cead8500e1c64d83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8309c1a4fd8f1a99165b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9942c722fc6dc47fb985",
+    "url": "/static/js/2.0f130e46.chunk.js"
+  },
+  {
+    "revision": "8309c1a4fd8f1a99165b",
+    "url": "/static/js/main.35453f4a.chunk.js"
+  },
+  {
+    "revision": "d190a40b18a723af82ea",
+    "url": "/static/js/runtime-main.68cca5f7.js"
+  }
+]);
